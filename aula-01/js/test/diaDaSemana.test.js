246c56b25a97a910c8e17164348798dfe5bd3946
J Victor Rocha <joaobrvictor@gmail.com>
2019-05-07 18:06:13 -0300

    Teste para o dia da semana

--- aula-01/js/test/diaDaSemana.test.js
+++ aula-01/js/test/diaDaSemana.test.js
@@ -1,23 +1,23 @@
 const funcao = require("../index.js");
 
-test("07/05/2019 retorna 1 (terça)", () => {
-    expect(funcao.diaDaSemana(7, 5, 2019)).toBe(1);
+test("07/05/2019 retorna 2 (terça)", () => {
+    expect(funcao.diaDaSemana(7, 5, 2019)).toBe(2);
 });
 
-test("06/05/2019 retorna 0 (satisfaz))", () => {
-    expect(funcao.diaDaSemana(6, 5, 2019)).toBe(0);
+test("06/05/2019 retorna 1 (segunda))", () => {
+    expect(funcao.diaDaSemana(6, 5, 2019)).toBe(1);
 });
 
 test("30/02/2019 dia nao satisfaz", () => {
-    expect(funcao.diaDaSemana(30, 2, 2019)).toBe(false);
+    expect(() => funcao.diaDaSemana(30, 2, 2019)).toThrow(RangeError);
 });
 
-test("01/30/2019 mês satisfaz", () => {
-    expect(funcao.diaDaSemana(1, 30, 2019)).toBe(false);
+test("01/30/2019 mês não satisfaz", () => {
+    expect(() => funcao.diaDaSemana(1, 30, 2019)).toThrow(RangeError);
 });
 
 test("01/30/1500 A função não aceita o ano 1500", () => {
-    expect(funcao.diaDaSemana(1, 30, 1500)).toBe(false);
+    expect(() => funcao.diaDaSemana(1, 30, 1500)).toThrow(RangeError);
 });
 
 test("argumento null/undefined", () => {
