6e96d4f38c541031d57c12c6c5a0383ebfccf50a
J Victor Rocha <joaobrvictor@gmail.com>
2019-05-07 18:09:34 -0300

    Simplificado a chamada

--- aula-01/js/test/diaDaSemana.test.js
+++ aula-01/js/test/diaDaSemana.test.js
@@ -1,44 +1,44 @@
-const funcao = require("../index.js");
+const funcao = require("../index.js").diaDaSemana;
 
 test("07/05/2019 retorna 2 (terça)", () => {
-    expect(funcao.diaDaSemana(7, 5, 2019)).toBe(2);
+    expect(funcao(7, 5, 2019)).toBe(2);
 });
 
 test("06/05/2019 retorna 1 (segunda))", () => {
-    expect(funcao.diaDaSemana(6, 5, 2019)).toBe(1);
+    expect(funcao(6, 5, 2019)).toBe(1);
 });
 
 test("30/02/2019 dia nao satisfaz", () => {
-    expect(() => funcao.diaDaSemana(30, 2, 2019)).toThrow(RangeError);
+    expect(() => funcao(30, 2, 2019)).toThrow(RangeError);
 });
 
 test("01/30/2019 mês não satisfaz", () => {
-    expect(() => funcao.diaDaSemana(1, 30, 2019)).toThrow(RangeError);
+    expect(() => funcao(1, 30, 2019)).toThrow(RangeError);
 });
 
 test("01/30/1500 A função não aceita o ano 1500", () => {
-    expect(() => funcao.diaDaSemana(1, 30, 1500)).toThrow(RangeError);
+    expect(() => funcao(1, 30, 1500)).toThrow(RangeError);
 });
 
 test("argumento null/undefined", () => {
     expect(() => {
-        funcao.diaDaSemana();
+        funcao();
     }).toThrow();
 });
 
 test("argumento nao numerico", () => {
     expect(() => {
-        funcao.diaDaSemana("abcd");
+        funcao("abcd");
     }).toThrow();
 });
 
 
 test("valor negativo fora da faixa", () => {
     expect(() => {
-        funcao.diaDaSemana(-1, 2, 2019);
+        funcao(-1, 2, 2019);
     }).toThrow(RangeError);
 });
 
 test("valor deve ser inteiro", () => {
-    expect(() => funcao.diaDaSemana(10.3, 5, 2019)).toThrow(RangeError);
+    expect(() => funcao(10.3, 5, 2019)).toThrow(RangeError);
 });
