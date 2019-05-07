61c7673c797559527c5dd48a0cf1a1fa4a6e5ed5
J Victor Rocha <joaobrvictor@gmail.com>
2019-05-07 18:49:12 -0300

    Teste do mod

---
+++ aula-01/js/test/mod.test.js
@@ -0,0 +1,29 @@
+const funcao = require("../index.js").mod;
+
+test("Caso válido - 8 dividido por 3", () => {
+    expect(funcao(8, 3)).toBe(2);
+});
+
+test("Dividendo menor que 0", () => {
+    expect(() => funcao(-1, 3)).toThrow(RangeError);
+});
+
+test("Divisor menor que 0", () => {
+    expect(() => funcao(8, -1)).toThrow(RangeError);
+});
+
+test("argumento null/undefined", () => {
+    expect(() => {
+        funcao();
+    }).toThrow();
+});
+
+test("argumento nao numerico", () => {
+    expect(() => {
+        funcao("abcd");
+    }).toThrow();
+});
+
+test("valor deve ser inteiro", () => {
+    expect(() => funcao(10.3, 5)).toThrow(RangeError);
+});
