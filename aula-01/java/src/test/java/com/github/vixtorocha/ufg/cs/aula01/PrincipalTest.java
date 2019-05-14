9740f601e73f10484e48cf8b8c463692d90c9913
J Victor Rocha <joaobrvictor@gmail.com>
2019-05-14 20:01:38 -0300

    Adequações no maven e no import

--- aula-01/java/src/test/java/com/github/vixtorocha/ufg/cs/aula01/PrincipalTest.java
+++ aula-01/java/src/test/java/com/github/vixtorocha/ufg/cs/aula01/PrincipalTest.java
@@ -1,7 +1,12 @@
 package com.github.vixtorocha.ufg.cs.aula01;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+// import static org.junit.Assert.*;
+// import org.junit.Test;
 
 public class PrincipalTest {
 
@@ -13,6 +18,11 @@ public class PrincipalTest {
     @Test
     public void propriedade3025isfalse() {
         assertEquals(false, Principal.propriedade3025(3024));
+    }
+
+    @Test
+    public void propriedade3025Pequeno() {
+        assertThrows(IllegalArgumentException.class, () -> Principal.propriedade3025(0));
     }
 
 }
