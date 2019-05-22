a02e71b0147c8a6e2b83495a763a5b9376a57785
vixtorocha <joaobrvictor@gmail.com>
2019-05-21 21:30:17 -0300

    package-info.java

---
+++ aula-01/java/src/main/java/com/github/vixtorocha/ufg/cs/aula01/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Pacote do repositório "cs-2019-01". Contém apenas uma classe: Principal, que
+ * em (a){@link com.github.vixtorocha.ufg.cs.aula01} em que há implementações de
+ * vários algoritmos matemáticos.
+ */
+
+package com.github.vixtorocha.ufg.cs.aula01;
