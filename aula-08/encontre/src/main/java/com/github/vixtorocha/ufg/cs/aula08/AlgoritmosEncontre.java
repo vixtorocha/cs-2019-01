21b4b8ccb0feb46859a982c6d90078d2a2cb565c
Joao Victor <joaobrvictor@gmail.com>
2019-06-10 15:17:30 -0300

    classes

---
+++ aula-08/encontre/src/main/java/com/github/vixtorocha/ufg/cs/aula08/AlgoritmosEncontre.java
@@ -0,0 +1,10 @@
+package com.github.vixtorocha.ufg.cs.aula08;
+
+public class AlgoritmosEncontre {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ aula-08/encontre/src/main/java/com/github/vixtorocha/ufg/cs/aula08/Principal.java
@@ -0,0 +1,8 @@
+package com.github.vixtorocha.ufg.cs.aula08;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+    }
+}
