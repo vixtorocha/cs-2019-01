a57c204d1e2ffbaa1d89a30591c674aa814ceb34
Joao Victor <joaobrvictor@gmail.com>
2019-06-28 17:19:55 -0300

    Algoritmos.qntdLetras

--- aula-07/vetor/java/src/main/java/com/github/vixtorocha/ufg/cs/aula07/Algoritmos.java
+++ aula-07/vetor/java/src/main/java/com/github/vixtorocha/ufg/cs/aula07/Algoritmos.java
@@ -13,21 +13,21 @@ public final class Algoritmos {
      */
     public static int menorTemperatura(final int[] vetTemp) {
 
-	if (vetTemp.length < 1) {
-	    throw new IllegalArgumentException(
-		    "Deve ser passado um vetor com pelo menos um elemento");
-	}
+        if (vetTemp.length < 1) {
+            throw new IllegalArgumentException(
+                    "Deve ser passado um vetor com pelo menos um elemento");
+        }
 
-	int menorTemp = vetTemp[0];
+        int menorTemp = vetTemp[0];
 
-	for (int currentTemp : vetTemp) {
+        for (int currentTemp : vetTemp) {
 
-	    if (currentTemp < menorTemp) {
-		menorTemp = currentTemp;
-	    }
-	}
+            if (currentTemp < menorTemp) {
+                menorTemp = currentTemp;
+            }
+        }
 
-	return menorTemp;
+        return menorTemp;
     }
 
     /**
@@ -38,21 +38,90 @@ public final class Algoritmos {
      */
     public static int somaImpares(final int[] vetNumeros) {
 
-	if (vetNumeros.length < 1) {
-	    throw new IllegalArgumentException(
-		    "Deve ser passado um vetor com pelo menos um elemento");
-	}
+        if (vetNumeros.length < 1) {
+            throw new IllegalArgumentException(
+                    "Deve ser passado um vetor com pelo menos um elemento");
+        }
 
-	int soma = 0;
+        int soma = 0;
 
-	for (int num : vetNumeros) {
-	    if (num % 2 != 0) {
-		soma += num;
-	    }
-	}
+        for (int num : vetNumeros) {
+            if (num % 2 != 0) {
+                soma += num;
+            }
+        }
 
-	return soma;
+        return soma;
     }
+
+    /**
+     * Conta a quantidade de elementos no primeiro vetor que está no segundo
+     * vetor.
+     * 
+     * @param vetor1
+     * @param vetor2
+     * @return A quantidade elementos do vetor1 repetido no vetor2
+     */
+    public static int qntElementosIguais(final int[] vetor1,
+            final int[] vetor2) {
+
+        int qntValoresIguais = 0;
+
+        for (int valor1 : vetor1) {
+            for (int valor2 : vetor2) {
+                if (valor1 == valor2) {
+                    qntValoresIguais++;
+                    break;
+                }
+            }
+        }
+
+        return qntValoresIguais;
+    }
+
+    /**
+     * Calcula a quantidade de cada letra em uma sequencia e armazena a
+     * quantidade em um vetor em que o indice corresponde com a posição da letra
+     * na sequencia.
+     * 
+     * @param sequencia
+     * @return O vetor com a quantidade de cada letra.
+     * @throws IllegalArgumentException se o argumento for uma sequencia vazia.
+     */
+    public static int[] qntdLetras(final String sequencia) {
+
+        if (sequencia.length() < 1) {
+            throw new IllegalArgumentException(
+                    "A sequencia não pode ser vazia.");
+        }
+
+        int[] vetorDeQuantidades = new int[sequencia.length()];
+
+        // zerar os valores do vetor de quantidade
+        for (int posicao : vetorDeQuantidades) {
+            posicao = 0;
+        }
+
+        for (int i = 0; i < sequencia.length(); i++) {
+
+            for (int o = i; o < sequencia.length(); o++) {
+                if (sequencia.charAt(i) == sequencia.charAt(o)) {
+                    vetorDeQuantidades[i]++;
+                    if (o != i) {
+                        vetorDeQuantidades[o]++;
+                    }
+                }
+            }
+        }
+
+        return vetorDeQuantidades;
+    }
+
+//    public static String palavraMaisFrequente(final String string) {
+//        String maisFrequente;
+//
+//        return maisFrequente;
+//    }
 
     /**
      * Método construtor privado para que a classe não possa ser instânciada.
--- aula-07/vetor/java/src/test/java/com/github/vixtorocha/ufg/cs/aula07/AlgoritmosTest.java
+++ aula-07/vetor/java/src/test/java/com/github/vixtorocha/ufg/cs/aula07/AlgoritmosTest.java
@@ -1,5 +1,6 @@
 package com.github.vixtorocha.ufg.cs.aula07;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -15,27 +16,27 @@ public class AlgoritmosTest {
      */
     @Test
     public void menorTempEh5() {
-	int vetor[] = {20, 5, 10, 8};
-	assertEquals(5, Algoritmos.menorTemperatura(vetor));
+        int vetor[] = {20, 5, 10, 8};
+        assertEquals(5, Algoritmos.menorTemperatura(vetor));
     }
 
     @Test
     public void menorTempNegativa() {
-	int vetor[] = {20, -3, 10, 8};
-	assertEquals(-3, Algoritmos.menorTemperatura(vetor));
+        int vetor[] = {20, -3, 10, 8};
+        assertEquals(-3, Algoritmos.menorTemperatura(vetor));
     }
 
     @Test
     public void tempUnica() {
-	int vetor[] = {20};
-	assertEquals(20, Algoritmos.menorTemperatura(vetor));
+        int vetor[] = {20};
+        assertEquals(20, Algoritmos.menorTemperatura(vetor));
     }
 
     @Test
     public void tempVazia() {
-	int vetor[] = {};
-	assertThrows(IllegalArgumentException.class,
-		() -> Algoritmos.menorTemperatura(vetor));
+        int vetor[] = {};
+        assertThrows(IllegalArgumentException.class,
+                () -> Algoritmos.menorTemperatura(vetor));
     }
 
     /**
@@ -43,14 +44,48 @@ public class AlgoritmosTest {
      */
     @Test
     public void somaCorreta() {
-	int vetor[] = {1, 2, 3, 4, 5, 6, 7, 8};
-	assertEquals(16, Algoritmos.somaImpares(vetor));
+        int vetor[] = {1, 2, 3, 4, 5, 6, 7, 8};
+        assertEquals(16, Algoritmos.somaImpares(vetor));
     }
 
     @Test
     public void vetorVazio() {
-	int vetor[] = {};
-	assertThrows(IllegalArgumentException.class,
-		() -> Algoritmos.somaImpares(vetor));
+        int vetor[] = {};
+        assertThrows(IllegalArgumentException.class,
+                () -> Algoritmos.somaImpares(vetor));
+    }
+
+    /**
+     * Testes para Algoritmo.qntElementosIguais
+     */
+    @Test
+    public void doisElementosRepetidos() {
+        int vetor1[] = {1, 2, 3, 4};
+        int vetor2[] = {1, 1, 2, 2};
+
+        assertEquals(2, Algoritmos.qntElementosIguais(vetor1, vetor2));
+    }
+
+    @Test
+    public void elementoVazio() {
+        int vetor[] = {};
+        assertThrows(IllegalArgumentException.class,
+                () -> Algoritmos.somaImpares(vetor));
+    }
+
+    /**
+     * Testes para Algoritmo.qntdLetras
+     */
+    @Test
+    public void qntdLetrasFuncionamentoCorreto() {
+        int[] resultadoEsperado = {5, 5, 1, 5, 3, 5, 3, 3, 5};
+        assertArrayEquals(resultadoEsperado,
+                Algoritmos.qntdLetras("AABACACCA"));
+    }
+
+    @Test
+    public void sequenciaVazia() {
+        assertThrows(IllegalArgumentException.class,
+                () -> Algoritmos.qntdLetras(""));
     }
 }
