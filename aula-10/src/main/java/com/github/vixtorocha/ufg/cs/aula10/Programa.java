d16c6658b618d1ab8486d674090f9799fee960ee
Joao Victor <joaobrvictor@gmail.com>
2019-07-02 21:48:36 -0300

    setup

---
+++ aula-10/src/main/java/com/github/vixtorocha/ufg/cs/aula10/Programa.java
@@ -0,0 +1,10 @@
+package com.github.vixtorocha.ufg.cs.aula10;
+
+public class Programa {
+
+    public static void main(String[] args) {
+        if(args.length() != )
+
+    }
+
+}
---
+++ aula-10/src/main/java/com/github/vixtorocha/ufg/cs/aula10/package-info.java
@@ -0,0 +1,1 @@
+package com.github.vixtorocha.ufg.cs.aula10;
