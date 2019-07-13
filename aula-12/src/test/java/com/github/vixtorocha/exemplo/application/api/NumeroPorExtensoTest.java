174f954cfa2376b19fdc10dda82fd2c8b4b16442
Joao Victor <joaobrvictor@gmail.com>
2019-07-13 13:35:23 -0300

    fix static reference

--- aula-12/src/test/java/com/github/vixtorocha/exemplo/application/api/NumeroPorExtensoTest.java
+++ aula-12/src/test/java/com/github/vixtorocha/exemplo/application/api/NumeroPorExtensoTest.java
@@ -4,18 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import com.github.vixtorocha.exemplo.application.api.NumeroPorExtenso;
-
 public class NumeroPorExtensoTest {
     NumeroPorExtenso numero = new NumeroPorExtenso(12, "Doze");
 
     @Test
     void getNumeroAbsoluto() {
-        assertEquals(12, NumeroPorExtenso.getNumeroArg());
+        assertEquals(12, numero.getNumeroArg());
     }
 
     @Test
     void getNumeroExtenso() {
-        assertEquals("Doze", NumeroPorExtenso.getNumeroPorExtenso());
+        assertEquals("Doze", numero.getNumeroPorExtenso());
     }
 }
