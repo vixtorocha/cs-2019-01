65e765e1338b5d78e659f3be3db79cb78abf49ee
Joao Victor <joaobrvictor@gmail.com>
2019-07-13 15:29:40 -0300

    Documentação do pacote mais clara. fixes #59

--- aula-12/src/main/java/com/github/vixtorocha/exemplo/domain/package-info.java
+++ aula-12/src/main/java/com/github/vixtorocha/exemplo/domain/package-info.java
@@ -1,4 +1,6 @@
 /**
- * Pacote de dominio para a implementação dos algoritmos.
+ * Pacote de dominio para a implementação dos algoritmos. Contem a classe
+ * Numeros, que implementa os métodos para obter a escrita de um número por
+ * extenso.
  */
 package com.github.vixtorocha.exemplo.domain;
